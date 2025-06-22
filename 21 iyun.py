2b36e1a8eafde441df4300601a1ea79b7c2bfa8f
Baxodirovich006 <bexruzjonraxmatov0454@gmail.com>
2025-06-22 15:40:31 +0500

    barcha python kodlarim

---
+++ 21 iyun.py
@@ -0,0 +1,67 @@
+
+
+def qosh(son):
+    print(f"{son} + 100 = {son+100}")
+    
+
+
+print(qosh(5))
+
+
+
+# kvadrat chiqaruvchi funksiya 
+def kop(x):
+    print(f"{x}*2 = {x**2}")
+    
+    
+    
+print(kop(4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
